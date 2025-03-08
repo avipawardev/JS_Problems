3e286538916c0d873547e403a1510900ded2016d
avipawardev <avipawar.dev@gmail.com>
2025-03-08 15:47:07 +0530

    Objects and Inbuild Method

---
+++ Objects-and-Inbuild/problem11.js
@@ -0,0 +1,8 @@
+let book = { title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937 };
+let str=""
+for(let keys in book){
+   str += `${keys}: ${book[keys]}`
+}
+console.log(str)
+//output 
+// title: The Hobbit author: J.R.R. Tolkien year: 1937
