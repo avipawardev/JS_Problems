f989f7538b81c42dd4e3899ab4ddab47e3a6e38b
avinash pawar <avipawar@avinashs-MacBook-Air.local>
2025-04-28 16:14:14 +0530

    layout-done

---
+++ Evaluation-5/problem-1/index.js
@@ -0,0 +1,7 @@
+
+let restaurant = [
+    { name: "Spice Garden", location: "Mumbai", orders: [400, 600, 800, 1200] },
+    { name: "Burger Hub", location: "Delhi", orders: [500, 700, 650, 550] },
+    { name: "Pasta Palace", location: "Bangalore", orders: [200, 300, 150, 250] }
+  ]
+
