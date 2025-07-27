0da625d190106b6e2928c8d8d625e58f26a73d82
avinash pawar <avipawar@avinashs-MacBook-Air.local>
2025-07-27 20:27:05 +0530

    introwithreact

---
+++ React/Week-1/Day-1/Introduction-to-React-with-JSX-Babel/problem-12/problem12/src/App.jsx
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>React</h2>
+     <h2>Javascript</h2>
+     <h2>CSS</h2>
+    </div>
+  );
+}
+
+export default App;
