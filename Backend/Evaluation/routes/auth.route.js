cf7e61bf3f3b77026b7927844e13aaa51a69a858
avipawardev <avipawar.dev@gmail.com>
2025-09-24 13:25:30 +0530

    authentication-is-done

--- Backend/Evaluation/routes/auth.route.js
+++ Backend/Evaluation/routes/auth.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const userModel = require('../models/user.model');
+const jwt = require('jsonwebtoken')
 
 const authRouter = express.Router();
 
@@ -12,6 +13,23 @@ authRouter.post('/register',async(req,res)=>{
         if(email && password){
             let user = await userModel.create({email,password:hashedPassword})
             res.status(201).json({message:'user registered successfully!',user})
+        }
+    } catch (error) {
+        res.status(401).json({message:error.message})
+    }
+})
+
+authRouter.post('/login',async(req,res)=>{
+    try {
+        const {email,password} = req.body;
+        let user = await userModel.findOne({email})
+        let match = await bcrypt.compare(password,user.password)
+        if(!user || !match){
+            return res.status(401).json({message:'something went wrong'})
+        }
+        else{
+            let token = await jwt.sign({userId:user._id},process.env.JWT_SECRET)
+            res.status(201).json({message:'user login successfully!',token,user})
         }
     } catch (error) {
         res.status(401).json({message:error.message})
